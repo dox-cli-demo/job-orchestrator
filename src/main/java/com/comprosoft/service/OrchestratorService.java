34473fba24fc233683ab2ad48e50262c79412288
Joby Pooppillikudiyil <joby81@gmail.com>
2025-07-04 09:54:58 +0200

    freemarker implementation

--- src/main/java/com/comprosoft/service/OrchestratorService.java
+++ src/main/java/com/comprosoft/service/OrchestratorService.java
@@ -1,10 +1,7 @@
 // src/main/java/com/example/orchestrator/service/OrchestratorService.java
 package com.comprosoft.service;
 
-import java.io.StringReader;
 import java.io.StringWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,18 +14,20 @@ import io.fabric8.kubernetes.api.model.batch.v1.Job;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.utils.Serialization;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class OrchestratorService {
-
-	private static final String TEMPLATE_PATH = "src/main/resources/templates/job-template.ftl";
 
 	private final Configuration freemarkerConfig;
 
 	public OrchestratorService() throws Exception {
 		freemarkerConfig = new Configuration(Configuration.VERSION_2_3_32);
+		freemarkerConfig.setClassForTemplateLoading(this.getClass(), "/templates");
 		freemarkerConfig.setDefaultEncoding("UTF-8");
 		freemarkerConfig.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
 	}
 
 	public void runJobTasks(Long requestId, int totaltasks) throws Exception {
@@ -37,22 +36,24 @@ public class OrchestratorService {
 		}
 	}
 
-	private void createJob(Long requestId, int taskId) throws Exception {
-		// Load FreeMarker template string from file
-		String templateContent = Files.readString(Paths.get(TEMPLATE_PATH));
+	public String renderToString(Long requestId, int taskId) throws Exception {
+		Template template = freemarkerConfig.getTemplate("job-template.ftl");
 
-		// Set template variables
 		Map<String, Object> dataModel = new HashMap<>();
 		dataModel.put("requestId", requestId);
 		dataModel.put("taskId", taskId);
 
-		// Create and process FreeMarker template
-		Template template = new Template("jobTemplate", new StringReader(templateContent), freemarkerConfig);
-		StringWriter renderedYaml = new StringWriter();
-		template.process(dataModel, renderedYaml);
+		try (StringWriter writer = new StringWriter()) {
+			template.process(dataModel, writer);
+			return writer.toString();
+		}
+	}
+
+	private void createJob(Long requestId, int taskId) throws Exception {
+		String jobTemplateYaml = renderToString(requestId, taskId);
 
 		// Deserialize rendered YAML to Fabric8 Job object
-		Job job = Serialization.unmarshal(renderedYaml.toString(), Job.class);
+		Job job = Serialization.unmarshal(jobTemplateYaml, Job.class);
 
 		// Create the job in Kubernetes
 		try (KubernetesClient client = new KubernetesClientBuilder().build()) {
@@ -61,7 +62,7 @@ public class OrchestratorService {
 				namespace = "default"; // fallback
 			}
 			client.resource(job).inNamespace(namespace).create();
-			System.out.println("Job created for taskId: " + taskId);
+			log.info("Job created for taskId: {} in namespace : {} ", taskId, namespace);
 		}
 	}
 }
